d1274f2573393da9c0e9cfb393249d67bd3ca685
DonJuan4-6 <diegohenao040@gmail.com>
2026-08-25 11:31:26 -0500

    feat: Se implementó un buscador de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import ProductoCard from './components/ProductoCard';
 import { productos } from './data/productos';
 import './App.css'
+import { useState } from 'react'; 
 
 function App() {
+  const [busqueda, setBusqueda] = useState("");
+  const productosFiltrados = productos.filter(producto =>
+              producto.nombre
+              .toLowerCase()
+              .includes(busqueda.toLowerCase())
+            );
+
   return (
     <main>
       <h1>Tienda tecnológica</h1>
-      {productos.map(producto => (
+      <input type="text"
+             placeholder="Buscar Producto..."
+             value={busqueda} 
+             onChange={(evento)=>{
+              setBusqueda(evento.target.value);
+             }}
+      />
+      {productosFiltrados.map(producto => (
         <ProductoCard
           key={producto.id}
           producto={producto}
         />
       ))}
+
+      {productosFiltrados.length === 0
+          ?<p>No se encontraron productos.</p>
+          : null
+        }
     </main>
   );
 }
